74f6df5d4efd462f80805661b8aecadfddae284d
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-12-02 23:27:30 -0300

    implementando estilos de texto

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,27 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Default App Structure",
-    home: Container(
-      color: Colors.blueAccent,
+    home: SafeArea(
+        child: Container(
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Flutter is Awesome",
+                textAlign: TextAlign.justify,
+                style: TextStyle(
+                  fontSize: 25.0,
+                  color: Colors.black,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none,
+                  letterSpacing: 1.5,
+                  wordSpacing: 1.5,
+                ),
+              ),
+          ],),
+        )
     ),
   ));
 }
