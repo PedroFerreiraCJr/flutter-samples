fd93a71ee1ffdc57a82ad53632ec96770e82a2a4
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-12-04 20:20:46 -0300

    implementando espaçamentos

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -5,20 +6,43 @@ void main() {
     title: "Default App Structure",
     home: SafeArea(
       child: Container(
-        color: Colors.white,
+        //color: Colors.white,
+        padding: EdgeInsets.all(5),
+        margin: EdgeInsets.all(5),
+        decoration: BoxDecoration(
+          border: Border.all(width: 3, color: Colors.white)
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              "Flutter is Awesome",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.black,
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.normal,
-                decoration: TextDecoration.none,
-                letterSpacing: 1.5,
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                "Flutter is Awesome",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none,
+                  letterSpacing: 1.5,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(5),
+              child: Text(
+                "Flutter is Awesome",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Colors.white,
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.normal,
+                  decoration: TextDecoration.none,
+                  letterSpacing: 1.5,
+                ),
               ),
             ),
           ],
