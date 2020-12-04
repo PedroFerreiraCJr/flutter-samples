9e74238be2af81cdf8540de49a252918b83a71bb
Pedro Ferreira <pedroferreiracjr@gmail.com>
2020-12-04 20:38:58 -0300

    implementando alinhamentos

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,51 @@
 import 'package:flutter/material.dart';
 
+///
+/// Elementos Principais:
+///   1. MainAxisAlignment;
+///   2. CrossAxisAlignment;
+///
 void main() {
   runApp(MaterialApp(
-    title: "Default App Structure",
+    title: "Alinhamentos",
     home: SafeArea(
       child: Container(
         color: Colors.white,
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: <Widget>[
-            Text(
-              "Flutter is Awesome",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 30,
-                color: Colors.black,
-                fontStyle: FontStyle.normal,
-                fontWeight: FontWeight.normal,
-                decoration: TextDecoration.none,
-                letterSpacing: 1.5,
-              ),
-            ),
+            text("Flutter"),
+            text("is"),
+            text("Awesome"),
           ],
         ),
       ),
     ),
   ));
+}
+
+///
+/// Forma de reaproveitar um Widget
+///
+Text text(final String value) {
+  return Text(
+    value,
+    textAlign: TextAlign.center,
+    style: style(),
+  );
+}
+
+///
+/// Default text style
+///
+TextStyle style() {
+  return TextStyle(
+    fontSize: 30,
+    color: Colors.black,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+    decoration: TextDecoration.none,
+    letterSpacing: 1.5,
+  );
 }
